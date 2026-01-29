84b15f4ab25bfbd63bbfe585b0f1e5227e2e354a
Dawuud <94dawmuk@skola.boras.se>
2026-01-29 16:13:30 +0100

    Added CatImage

--- CatController.cs
+++ CatController.cs
@@ -5,16 +5,25 @@ using Microsoft.AspNetCore.Mvc;
 [Route("api/v1/cats")]
 public class CatController : ControllerBase
 {
+    private readonly string[] _catImage = new[]
+    {
+        "/cats/cat1.png",
+        "/cats/cat2.png",
+        "/cats/cat3.png"
+    };
     [HttpGet("random")]
     public IActionResult GetRandomCat()
     {
+        var random = new Random();
+        var randomImagePath = _catImage[random.Next(_catImage.Length)];
         var response = new
         {
-            url = "https://cataas.com/cat",
+            url = randomImagePath,
             width = 300,
             height = 200,
             message = "Här är en fin katt!"
         };
         return Ok(response);
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,7 +1,30 @@
 
+using Microsoft.AspNetCore.Mvc;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Lägg till CORS för att tillåta access från webbsidor (viktigt för uppgiften)
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader();
+    });
+});
+
 builder.Services.AddControllers();
+
 var app = builder.Build();
 
+// Aktivera CORS
+app.UseCors();
+
+// Aktivera static files (för att servera katbilder från wwwroot-mappen)
+app.UseStaticFiles();
+
+// Mappa controllers
 app.MapControllers();
+
 app.Run();
